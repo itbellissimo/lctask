941ba0b6c1786c7f48db458602a9471cfa63a75f
Sergei Kuznetsov <s.kuznecov@city-mobil.ru>
2022-12-04 23:30:01 +0300

    [task-142] Linked List Cycle II. Floyed algo

--- task142/main.go
+++ task142/main.go
@@ -29,6 +29,8 @@ func main() {
 	fmt.Println(res)
 	i := detectCycleIndex(head)
 	fmt.Println(i)
+	floydAlgoRes := detectCycleFloyds(head)
+	fmt.Println(floydAlgoRes)
 }
 
 // ListNode Definition for singly-linked list.
@@ -78,4 +80,36 @@ func detectCycleIndex(head *ListNode) int {
 	}
 
 	return -1
+}
+
+// https://leetcode.com/problems/linked-list-cycle-ii/discuss/726011/A-mechanics-based-visualization-(%2B-Floyds-algo-explained)
+func detectCycleFloyds(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if fast == slow {
+			break
+		}
+	}
+	if fast != slow || head == nil || head.Next == nil {
+		return nil
+	}
+	count := 1
+	curr := fast.Next
+	for curr != fast {
+		curr = curr.Next
+		count++
+	}
+	//count === len of the loop
+	front, rear := head, head
+	for i := 0; i < count; i++ {
+		front = front.Next //get a distance of L between them
+	}
+	for front != rear { //keep moving both at same rate until they meet
+		front = front.Next
+		rear = rear.Next
+	}
+	return rear //or front
+
 }
